a23c13f908ebe41dc8382f063fa9033fd957fe90
James Acero <jacero43464@liceo.edu.ph>
2026-03-16 20:01:58 +0800

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   standalone: true,
-  templateUrl: './header.component.html', // Dapat 'header'
-  styleUrls: ['./header.css']   // Dapat 'header'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.css'] 
 })
 export class HeaderComponent { }
